451ea1b21f9846727bfc60a920754593d7eb0bd7
Rafael <webcodejunkie@gmail.com>
2021-05-19 18:45:18 -0400

    3 objects nested in pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,22 @@
-let pokemonList = [];
+let pokemonList = [
+  {
+    name: 'Arcanine',
+    height: 6,
+    types: [fire],
+    category: 'legendary'
+  },
+
+  {
+    name: 'Gengar',
+    height: 5,
+    types: [ghost, poison],
+    category: 'shadow'
+  },
+
+  {
+    name: 'Eevee',
+    height: 1,
+    types: [normal],
+    category: 'evolution'
+  }
+];
