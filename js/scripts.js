b36624f205602a6d152fbf4c24fa44fa3e08e432
webcodejunkie <webcodejunkie@gmail.com>
2021-05-24 13:02:29 -0400

    replacement of forEach function

--- js/scripts.js
+++ js/scripts.js
@@ -21,11 +21,6 @@
       }
     ];
 
-    let thisPokemonIS = 'This Pokemon\'s name is—';
-    let thisPokemonHeight = 'It\'s height is: ';
-    let bigPokemon = '<span class="bigP"> - This is a big Pokemon! </span>';
-    let smallPokemon = '<span class="smallP"> - This is a small Pokemon </span>';
-
     // function to add new Pokemon's to the pokemonList array & also check to make sure it adds an object rejecting all other types of
     function add(pokemon){
       if (typeof (pokemon) === 'object') {
@@ -41,22 +36,21 @@
       return pokemonList;
     }
 
-      // A forEach function that'll replace a for loop
-    pokemonList.forEach(function(pokemon){
-      // a series of template literals being used for cleaner code
-      document.write( `${thisPokemonIS}` + pokemon.name + `${thisPokemonHeight}` + pokemon.height);
-      // A conditional that will return a string to whether or not a Pokemon is big or small
-      if (pokemon.height <= 3) {
-        document.write(`${smallPokemon}`);
-      } else if (pokemon.height > 3 && pokemon.height <= 10) {
-        document.write(`${bigPokemon}`);
-      }
-      // Linebreak after every iteration
-      document.write('<br>');
-    });
-
     return {
       add: add,
       getAll: getAll
     };
   })();
+
+  // a forEach function that'll replace a for loop
+  pokemonRepository.getAll().forEach(function(pokemon){
+    document.write(`This Pokemon is — ${pokemon.name} it\'s height is (${pokemon.height})—`);
+    // A conditional that will return a string to whether or not a Pokemon is big or small
+    if (pokemon.height <= 3) {
+      document.write('<span class="smallP">' + 'This is a small Pokemon' + '</span>');
+    } else if (pokemon.height > 3 && pokemon.height <= 10) {
+      document.write('<span class="bigP">' + 'This is a big Pokemon' + '</span>');
+      // Linebreak after every iteration
+      document.write('<br>');
+    }
+  });
